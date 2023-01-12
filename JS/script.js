14839781902ef14f6ebb2fa703de12fa6fa68621
marcos ventura <marcosventura.dev@gmail.com>
2023-01-11 21:40:12 -0300

    atribuindo funções de movimento ao personagem

--- JS/script.js
+++ JS/script.js
@@ -24,6 +24,7 @@ class Player{
 //função que acrescenta +1px no eixo y
     update(){
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         if (this.position.y + this.height + this.velocity.y < canvas.height)
             this.velocity.y += gravity
@@ -41,6 +42,17 @@ const player2 = new Player({
 })
 
 
+//atribuindo valores boleano à constante keys para dise ser determinado botão foi pressionado ou não
+const keys ={
+    d:{
+        pressed: false,
+    },
+    a:{
+        pressed: false,
+    },
+}
+
+
 /* let y = 100
 let y2 = 100 */
 
@@ -55,7 +67,40 @@ function animate() {
 
     player.update()
     player2.update()
+
+    player.velocity.x = 0
+
+    //se o botão 'd' estiver sendo pressionado então atribuir à variavel x da função velocidade +1 px 
+    //se não é subitraído -1 a cada frame
+    if(keys.d.pressed)player.velocity.x = 1
+        else if(keys.a.pressed)player.velocity.x = -1
 }
 
 animate()
 
+//metodo de controle usando switch case 
+window.addEventListener('keydown', (event)=>{
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = true
+            break
+        case 'a':
+            keys.a.pressed = true
+             break
+        case 'w':
+            player.velocity.y = -5
+             break
+    }
+})
+
+window.addEventListener('keyup', (event)=>{
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+             break
+      
+    }
+})

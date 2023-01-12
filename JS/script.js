8442c6945bafedeaac4e2de8207749694cc0e81c
marcos ventura <marcosventura.dev@gmail.com>
2023-01-11 22:10:05 -0300

    implementando background

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,32 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+const scaledCanvas ={
+    width: canvas.width / 4,
+    height: canvas.height / 4
+}
+
 const gravity = 0.5
+
+
+//implementando Sprites/ background
+class Sprite{
+    constructor({position, imageSrc}){
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+    draw(){
+        if(!this.image)return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+    update(){
+        this.draw()
+    }
+}
+
+
 
 class Player{
     constructor(position){
@@ -52,6 +77,13 @@ const keys ={
     },
 }
 
+const background = new Sprite({
+    position:{
+        x: 0,
+        y: 0,
+    },
+    imageSrc:'../img/background.png',
+})
 
 /* let y = 100
 let y2 = 100 */
@@ -65,6 +97,13 @@ function animate() {
     //criando espaçamento 0 nos eixos x e y e recebendo  width e height dos valores atribuidos antes
     c.fillRect(0, 0, canvas.width, canvas.height)
 
+    c.save()//
+    c.scale(4, 4) //altera as dimensões da imagem 
+    c.translate(0, -background.image.height + scaledCanvas.height)//
+    background.update()
+    c.restore()//
+
+    
     player.update()
     player2.update()
 

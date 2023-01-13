5c60d2193255fe4d4cdd9ed8be69c0b618fb733f
marcos ventura <marcosventura.dev@gmail.com>
2023-01-13 08:43:47 -0300

    novas funcionalidade

---
+++ JS/classes/Sprite.js
@@ -0,0 +1,16 @@
+//implementando Sprites/ background
+class Sprite{
+    constructor({position, imageSrc}){
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+    draw(){
+        if(!this.image)return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+    update(){
+        this.draw()
+    }
+}
--- JS/index.js
+++ JS/index.js
@@ -4,85 +4,65 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-const scaledCanvas ={
+const scaledCanvas = {
     width: canvas.width / 4,
     height: canvas.height / 4
 }
 
+
+const floorCollisions2D = []
+for (let i = 0; i < floorCollisions.length; i += 36) {
+    floorCollisions2D.push(floorCollisions.slice(i, i + 36))
+}
+
+const collisionBlock = []
+floorCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol === 1729) {
+            console.log('draw a collisions')
+            collisionBlock.push(new CollisionBlock({
+                position: {
+                    x: x * 16,
+                    y: y * 16,
+                }
+            }))
+
+        }
+    })
+})
+
+console.log(collisionBlock)
+
 const gravity = 0.5
 
 
-//implementando Sprites/ background
-class Sprite{
-    constructor({position, imageSrc}){
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
-    }
-    draw(){
-        if(!this.image)return
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
-
-    update(){
-        this.draw()
-    }
-}
-
-
-
-class Player{
-    constructor(position){
-        this.position = position
-        this.velocity = {
-            x:0,
-            y:1
-        }
-        this.height = 100
-    }
-
-    //cria o player definindo os eixos gerado pelo constructor e o tamanho
-    draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 100, this.height)
-    }
-//função que acrescenta +1px no eixo y
-    update(){
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if (this.position.y + this.height + this.velocity.y < canvas.height)
-            this.velocity.y += gravity
-        else this.velocity.y = 0
-    }
-}
 
 const player = new Player({
-    x:0,
-    y:0,
+    x: 0,
+    y: 0,
 })
 const player2 = new Player({
-    x:300,
-    y:0,
+    x: 300,
+    y: 0,
 })
 
 
 //atribuindo valores boleano à constante keys para dise ser determinado botão foi pressionado ou não
-const keys ={
-    d:{
+const keys = {
+    d: {
         pressed: false,
     },
-    a:{
+    a: {
         pressed: false,
     },
 }
 
 const background = new Sprite({
-    position:{
+    position: {
         x: 0,
         y: 0,
     },
-    imageSrc:'../img/background.png',
+    imageSrc: '../img/background.png',
 })
 
 /* let y = 100
@@ -98,12 +78,16 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
 
     c.save()//
-    c.scale(4, 4) //altera as dimensões da imagem 
+    c.scale(1,1) //altera as dimensões da imagem 
     c.translate(0, -background.image.height + scaledCanvas.height)//
     background.update()
+    collisionBlock.forEach((collisionBlock) =>{
+        collisionBlock.update()
+    })
     c.restore()//
 
-    
+
+
     player.update()
     player2.update()
 
@@ -111,35 +95,35 @@ function animate() {
 
     //se o botão 'd' estiver sendo pressionado então atribuir à variavel x da função velocidade +1 px 
     //se não é subitraído -1 a cada frame
-    if(keys.d.pressed)player.velocity.x = 1
-        else if(keys.a.pressed)player.velocity.x = -1
+    if (keys.d.pressed) player.velocity.x = 1
+    else if (keys.a.pressed) player.velocity.x = -1
 }
 
 animate()
 
 //metodo de controle usando switch case 
-window.addEventListener('keydown', (event)=>{
-    switch(event.key){
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
         case 'd':
             keys.d.pressed = true
             break
         case 'a':
             keys.a.pressed = true
-             break
+            break
         case 'w':
             player.velocity.y = -5
-             break
+            break
     }
 })
 
-window.addEventListener('keyup', (event)=>{
-    switch(event.key){
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
         case 'd':
             keys.d.pressed = false
             break
         case 'a':
             keys.a.pressed = false
-             break
-      
+            break
+
     }
 })
